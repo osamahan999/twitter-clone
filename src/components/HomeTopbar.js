286d21322939b6b88e308547991a5a2fbe5584b5
Osama Hanhan <hanhan.osama@gmail.com>
2020-10-30 02:29:29 -0700

    did more shit

--- src/components/HomeTopbar.js
+++ src/components/HomeTopbar.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 
 import '../css/TwitterFeed.css';
- 
+
 
 function HomeTopBar() {
-  return (
-    <div className="HomeTopBar">
-        <p id="HomeText">Home</p>
-        <div id = "HomeStarIcon">HI</div>
-    </div>
-  );
+    return (
+        <div className="HomeTopBar">
+            <p id="HomeText">Home</p>
+            <p id="HomeStarIcon">Star</p>
+        </div>
+    );
 }
 
 export default HomeTopBar
