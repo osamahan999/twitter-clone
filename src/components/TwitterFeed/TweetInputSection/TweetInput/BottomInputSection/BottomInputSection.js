ce9a26c7b730e187f81828524fb44439ad93c499
Osama Hanhan <hanhan.osama@gmail.com>
2020-10-31 19:04:08 -0700

    added the icons to the textbox and the border.

---
+++ src/components/TwitterFeed/TweetInputSection/TweetInput/BottomInputSection/BottomInputSection.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import styles from './BottomInputSection.module.css';
+
+function BottomInputSection() {
+    return (
+        <div className={styles.BottomInputSection}>
+            <div className={styles.icons}>
+                <p>icon</p>
+                <p>icon</p>
+                <p>icon</p>
+                <p>icon</p>
+                <p>icon</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default BottomInputSection
--- src/components/TwitterFeed/TweetInputSection/TweetInput/TweetInput.js
+++ src/components/TwitterFeed/TweetInputSection/TweetInput/TweetInput.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 
 import styles from './TweetInput.module.css';
+import BottomInputSection from './BottomInputSection/BottomInputSection';
 
 function TweetInput() {
     return (
-        <div className={styles.TweetInput}>
-            <textarea type="text" className={styles.textbox} placeholder="What's Happening?"></textarea>
+        <div>
+
+
+
+            <div className={styles.TweetInput}>
+                <textarea type="text" className={styles.textbox} placeholder="What's happening?"></textarea>
+            </div>
+
+            <BottomInputSection />
         </div>
     );
 }
