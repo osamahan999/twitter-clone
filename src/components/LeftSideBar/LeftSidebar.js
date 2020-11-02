56cc3fd16257fea4424b5f07797ac25afec51c75
slouderback <sam@louderback.com>
2020-11-01 21:42:09 -0800

    left sidebare and backend

--- src/components/LeftSideBar/LeftSidebar.js
+++ src/components/LeftSideBar/LeftSidebar.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-import TwitterIcon from '@material-ui/icons/Twitter';
+import Modal from "@material-ui/core/Modal";
+
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 import SidebarOption from "./SidebarOption/SidebarOption";
 import HomeIcon from "./Icons/HomeIcon";
@@ -15,18 +17,65 @@ import MoreIcon from "./Icons/MoreIcon";
 import styles from "./LeftSidebar.module.css";
 
 function LeftSidebar() {
+	const [open, setOpen] = React.useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	return (
 		<div className={styles.container}>
-			<TwitterIcon className={styles.twitterIcon} style={{ fontSize: 35 }}/>
-			<SidebarOption Icon={HomeIcon} text="Home" />
-			<SidebarOption Icon={ExploreIcon} text="Explore" />
-			<SidebarOption Icon={NotificationsIcon} text="Notifications" />
-			<SidebarOption Icon={MessagesIcon} text="Messages" />
-			<SidebarOption Icon={BookmarkIcon} text="Bookmarks" />
-			<SidebarOption Icon={ListIcon} text="Lists" />
-			<SidebarOption Icon={ProfileIcon} text="Profile" />
-			<SidebarOption Icon={MoreIcon} text="More" />
-			<button className={styles.tweetButton}>Tweet</button>
+			<TwitterIcon className={styles.twitterIcon} style={{ fontSize: 35 }} />
+			<SidebarOption
+				Icon={HomeIcon}
+				text="Home"
+				handlerFunction={() => console.log("Navigate to home")}
+			/>
+			<SidebarOption
+				Icon={ExploreIcon}
+				text="Explore"
+				handlerFunction={() => console.log("Navigate to explore")}
+			/>
+			<SidebarOption
+				Icon={NotificationsIcon}
+				text="Notifications"
+				handlerFunction={() => console.log("Navigate to notifications")}
+			/>
+			<SidebarOption
+				Icon={MessagesIcon}
+				text="Messages"
+				handlerFunction={() => console.log("Navigate to messages")}
+			/>
+			<SidebarOption
+				Icon={BookmarkIcon}
+				text="Bookmarks"
+				handlerFunction={() => console.log("Navigate to bookmarks")}
+			/>
+			<SidebarOption
+				Icon={ListIcon}
+				text="Lists"
+				handlerFunction={() => console.log("Navigate to lists")}
+			/>
+			<SidebarOption
+				Icon={ProfileIcon}
+				text="Profile"
+				handlerFunction={() => console.log("Navigate to profile")}
+			/>
+			<SidebarOption
+				Icon={MoreIcon}
+				text="More"
+				handlerFunction={() => console.log("Navigate to more stuff")}
+			/>
+			<button className={styles.tweetButton} onClick={handleOpen}>
+				Tweet
+			</button>
+			<Modal open={open} onClose={handleClose} className={styles.tweetModal}>
+				<div className={styles.tweetModalContent}>Tweet</div>
+			</Modal>
 		</div>
 	);
 }
--- src/components/LeftSideBar/SidebarOption/SidebarOption.js
+++ src/components/LeftSideBar/SidebarOption/SidebarOption.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-import styles from "./SidebarOption.module.css"
+import styles from "./SidebarOption.module.css";
 
-function SidebarOption({ Icon, text }) {
+function SidebarOption({ Icon, text, handlerFunction }) {
 	return (
-		<div className={styles.container}>
-			<Icon className={styles.icon}/>
+		<div className={styles.container} onClick={handlerFunction}>
+			<Icon className={styles.icon} />
 			<h3>{text}</h3>
 		</div>
 	);
